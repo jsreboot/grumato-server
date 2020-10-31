e2013b8c63df61c35d50f13826dd54010dc579c3
Artur Vincho <amarkusheuski@jaydevs.com>
2020-11-01 00:42:10 +0300

    Add logic chek user

--- src/main/java/com/example/DAO/impl/LoginDaoImpl.java
+++ src/main/java/com/example/DAO/impl/LoginDaoImpl.java
@@ -3,19 +3,23 @@ package com.example.DAO.impl;
 import com.example.DAO.LoginDao;
 import com.example.db.HibernateUtil;
 import com.example.testEntity.Login;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
-import java.sql.SQLException;
+import javax.transaction.Transactional;
+import java.util.List;
 
 public class LoginDaoImpl implements LoginDao {
     @Override
     public String createLogin(Login login) {
         String result = "";
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
         try {
-            session.beginTransaction();
             session.save(login);
             session.getTransaction().commit();
+            session.close();
             result = "success";
         } catch (Exception e) {
             System.out.println("error message: " + e);
@@ -26,15 +30,17 @@ public class LoginDaoImpl implements LoginDao {
         return result;
     }
 
+    @Transactional()
     @Override
     public String readLogin(Login login) {
-        String result = "";
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        String result = "false";
+        Session session = HibernateUtil.getSessionFactory().openSession();
         try {
             session.beginTransaction();
-            session.save(login);
-            session.getTransaction().commit();
-            result = "success";
+            Criteria criteria = session.createCriteria(Login.class).add(Restrictions.and(Restrictions.eq("userName", login.getUserName()),
+                    Restrictions.eq("userPassword", login.getUserPassword())));
+            List<Login> list = criteria.list();
+            return list.size() > 0 ? "true" : "false";
         } catch (Exception e) {
             System.out.println("error message: " + e);
             result = "abort " + e;
